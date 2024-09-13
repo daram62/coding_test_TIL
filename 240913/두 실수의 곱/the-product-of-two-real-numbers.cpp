4f9e1c5dfa4c9ea751ac872769932245040bf230
Minseo Kim <marimang0602@g.skku.edu>
2024-09-13 16:20:38 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240913/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    float a, b, res;
+
+    a = 5.26;
+    b = 8.27;
+    res = a * b;
+    
+    printf("%.3f", res);
+    return 0;
+}
