8ff4931046f4d2e927af7313849d321a2c809024
Minseo Kim <marimang0602@g.skku.edu>
2024-09-15 22:33:53 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int fir, sec, thr;
+
+    cin >> fir >> sec >> thr;
+
+    cout << "010" << thr << sec;
+
+    return 0;
+}
