0fa046edca1ccbbc1ef81550e5f2f87006c1a5ea
Minseo Kim <marimang0602@g.skku.edu>
2024-09-18 23:59:17 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240918/두 사람/two-person.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int age[2];
+    char gender[2];
+
+    cin >> age[0] >> gender[0];
+    cin >> age[1] >> gender[1];
+
+    if (age[0] >= 19 || age[1] >= 19) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
