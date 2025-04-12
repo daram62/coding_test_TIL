83298fde4a97486c92ccc4a2a7ea10d6c0f70d6d
Minseo Kim <marimang0602@g.skku.edu>
2025-04-12 19:32:19 +0900

    Title: 메두사와 전사들, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 250412/메두사와 전사들/medusa-and-warriors.cpp
+++ 250412/메두사와 전사들/medusa-and-warriors.cpp
@@ -1,296 +1,312 @@
-#include <iostream>
-#include <queue>
-#include <cmath>
-#include <cstring>
-#include <tuple>
+#include <bits/stdc++.h>
 using namespace std;
-
-int N, M;
-int map_[50][50];
-int warriorMap[50][50], nextWarriorMap[50][50];
-bool medusaArea[4][50][50];
-
-int dr[4] = {-1, 1, 0, 0};
-int dc[4] = {0, 0, -1, 1};
-
-int medusaR, medusaC;
-int parkR, parkC;
-
-pair<int, int> warriorsQueue[10000];
-int frontIdx = 0, rearIdx = 0;
-
-int getDist(int r1, int c1, int r2, int c2) {
-    return abs(r1 - r2) + abs(c1 - c2);
+typedef long long ll;
+typedef pair<int, int> pii;
+void fastIO() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(NULL);
+  cout.tie(NULL);
 }
 
-bool inRange(int r, int c) {
-    return r >= 0 && r < N && c >= 0 && c < N;
+typedef struct _Warrior
+{
+  int y, x, freeze, live;
+}Warrior;
+
+vector<Warrior> warrior_vec;
+int park_dist[54][54]; // medusa에서 park까지의 최단거리
+int visited[4][54][54];
+
+// 북쪽이 0, 이후 시계방향으로 1,2, ... 7
+const int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+// (상, 하, 좌, 우) 에 대한 탐색방향
+// ex 상(idx == 0) 일 경우 탐색방향은 
+// (7, 0, 1) <- (북서, 북, 북동)
+vector<vector<int>> dirs = {
+  {7, 0, 1}, // 기울기 -> (양수, 0또는 무한대, 음수)
+  {3, 4, 5}, // 기울기 -> (양수, 0또는 무한대, 음수)
+  {7, 6, 5}, // 기울기 -> (양수, 0또는 무한대, 음수)
+  {3, 2, 1}  // 기울기 -> (양수, 0또는 무한대, 음수)
+};
+
+int n,m;
+int my, mx, py, px; // medusa, park
+int road[54][54];
+int warrior_land[54][54];
+
+typedef struct _Info
+{
+  int warrior_move_sum;
+  int warrior_freezed;
+  int warrior_attack_succeed;
+} Info;
+
+Info turn_info;
+
+bool is_out(int y, int x)
+{
+  return (y < 0 || y >= n || x < 0 || x >= n);
 }
 
-void pushWarrior(int r, int c) {
-    warriorsQueue[rearIdx++] = {r, c};
-}
+int calc_dist()
+{
+  memset(park_dist, -1, sizeof(park_dist));
+  queue <pii> q;
+  q.push({py, px});
+  park_dist[py][px] = 0;
 
-bool popWarrior(int &r, int &c) {
-    if (frontIdx == rearIdx) return false;
-    tie(r, c) = warriorsQueue[frontIdx++];
-    return true;
-}
+  while(q.size())
+  {
+    pii here = q.front(); q.pop();
+    int y = here.first;
+    int x = here.second;
 
-void resetWarriorQueue(pair<int, int>* src, int count) {
-    for (int i = 0; i < count; i++) {
-        warriorsQueue[i] = src[i];
+    if (y == my && x == mx) return park_dist[y][x];
+    for (int i = 0; i < 8; i += 2)
+    {
+      int ny = y + dy[i];
+      int nx = x + dx[i];
+
+      if (is_out(ny, nx) || road[ny][nx] == 1) continue;
+
+      if (park_dist[ny][nx] == -1)
+      {
+        park_dist[ny][nx] = park_dist[y][x] + 1;
+        q.push({ny, nx});
+      }
     }
-    frontIdx = 0;
-    rearIdx = count;
+  }
+  return -1;
 }
 
-bool step1(bool visited[50][50]) {
-    int minDist = 1e9, chosenDir = -1, newR = -1, newC = -1;
+vector<pii> path;
 
-    for (int d = 0; d < 4; d++) {
-        int nr = medusaR + dr[d];
-        int nc = medusaC + dc[d];
-        if (!inRange(nr, nc) || visited[nr][nc] || map_[nr][nc] == 1) continue;
+bool trace(int y, int x) {
+  if (y == py && x == px) return true;
 
-        bool bfsVisited[50][50] = {};
-        queue<tuple<int, int, int>> q;
-        q.push({0, nr, nc});
-        bfsVisited[nr][nc] = true;
+  for (int i = 0; i < 4; i++)
+  {
+    int dir = dirs[i][1];
+    int ny = y + dy[dir];
+    int nx = x + dx[dir];
+    if (is_out(ny, nx) || park_dist[ny][nx] == -1) continue;
+    if (park_dist[y][x] == park_dist[ny][nx] + 1)
+    {
+      path.push_back({ny, nx});
+      if (trace(ny, nx)) return true;
+      path.pop_back();
+    }
+  }
+  
+  return false;
+}
 
-        while (!q.empty()) {
-            auto [dist, r, c] = q.front(); q.pop();
-            if (r == parkR && c == parkC) {
-                if (dist < minDist || (dist == minDist && d < chosenDir)) {
-                    minDist = dist;
-                    chosenDir = d;
-                    newR = nr;
-                    newC = nc;
-                }
-                break;
+void medusa_move(int turn)
+{
+  tie(my, mx) = path[turn];
+  for (Warrior& w : warrior_vec)
+  {
+    if (w.live == 0) continue;
+    if (w.y == my && w.x == mx)
+    {
+      w.live = 0;
+      warrior_land[w.y][w.x] -= 1;
+    }
+  }
+  return;
+}
+
+int max_freeze_dir;
+void medusa_look()
+{
+  memset(visited, 0, sizeof(visited));
+  int max_freeze_cnt = 0;
+  for (int i = 0; i < 4; i++)
+  {
+    vector<int> look_dir = dirs[i];
+    queue<pii> q;
+    int freeze_cnt = 0;
+    q.push({my, mx});
+    while(q.size())
+    {
+      pii here = q.front(); q.pop();
+      int y = here.first;
+      int x = here.second;
+      for (int dir : look_dir)
+      {
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if (is_out(ny, nx) || visited[i][ny][nx]) continue;
+        if (warrior_land[ny][nx])
+        {
+          freeze_cnt += warrior_land[ny][nx];
+          visited[i][ny][nx] = 1;
+          queue<pii> warrior_q;
+          vector<int> warrior_dir;
+          if (ny == my || nx == mx)
+          {
+            warrior_dir.push_back(look_dir[1]);
+          }
+          else
+          {
+            if ( (double)(ny - my) / (double)(nx - mx) > 0)
+            {
+              warrior_dir.push_back(look_dir[0]);
+              warrior_dir.push_back(look_dir[1]);
             }
-            for (int i = 0; i < 4; i++) {
-                int rr = r + dr[i], cc = c + dc[i];
-                if (!inRange(rr, cc) || map_[rr][cc] == 1 || bfsVisited[rr][cc]) continue;
-                bfsVisited[rr][cc] = true;
-                q.push({dist + 1, rr, cc});
+            else
+            {
+              warrior_dir.push_back(look_dir[1]);
+              warrior_dir.push_back(look_dir[2]);
             }
+          }
+          warrior_q.push({ny, nx});
+          while(warrior_q.size())
+          { 
+            pii here = warrior_q.front(); warrior_q.pop();
+            int y = here.first;
+            int x = here.second;
+            for (int dir : warrior_dir)
+            {
+              int ny = y + dy[dir];
+              int nx = x + dx[dir];
+              if (is_out(ny, nx) || visited[i][ny][nx]) continue;
+              visited[i][ny][nx] = 100;
+              warrior_q.push({ny, nx});
+            }
+          }
         }
+        else
+        {
+          q.push({ny, nx});
+          visited[i][ny][nx] = 1;
+        }
+      }
     }
 
-    if (chosenDir == -1) {
-        cout << -1 << '\n';
-        return false;
+    if (max_freeze_cnt < freeze_cnt)
+    {
+      max_freeze_cnt = freeze_cnt;
+      max_freeze_dir = i;
     }
+  }
 
-    medusaR = newR;
-    medusaC = newC;
-    visited[medusaR][medusaC] = true;
-
-    pair<int, int> temp[10000];
-    int cnt = 0;
-    for (int i = frontIdx; i < rearIdx; i++) {
-        auto [r, c] = warriorsQueue[i];
-        if (r == medusaR && c == medusaC) continue;
-        temp[cnt++] = {r, c};
+  for (Warrior& w : warrior_vec)
+  {
+    if (w.live == 0) continue;
+    if (visited[max_freeze_dir][w.y][w.x] == 1)
+    {
+      w.freeze = 1;
+      turn_info.warrior_freezed++;
     }
-    resetWarriorQueue(temp, cnt);
-    warriorMap[medusaR][medusaC] = 0;
-
-    if (medusaR == parkR && medusaC == parkC) {
-        cout << 0 << '\n';
-        return false;
-    }
-
-    return true;
+  }
 }
 
-pair<int, int> step2() {
-    int maxKill = 0, dirChosen = 0;
-    memset(medusaArea, false, sizeof(medusaArea));
-
-    for (int d = 0; d < 4; d++) {
-        bool rock[50] = {};
-        int kill = 0;
-
-        for (int step = 1; step < N; step++) {
-            int r = medusaR + step * dr[d];
-            int c = medusaC + step * dc[d];
-            if (!inRange(r, c)) break;
-
-            medusaArea[d][r][c] = true;
-            if (warriorMap[r][c] > 0) {
-                if (d <= 1) rock[c] = true;
-                else rock[r] = true;
-                kill += warriorMap[r][c];
-                break;
-            }
-        }
-
-        int rr = medusaR, cc = medusaC;
-        while (inRange(rr, cc)) {
-            bool left = false, right = false;
-            for (int j = 1; j <= N && (!left || !right); j++) {
-                int r = (d <= 1 ? rr + j * dr[d] : rr);
-                int c = (d <= 1 ? cc : cc + j * dc[d]);
-                if (!inRange(r, c)) break;
-
-                if (d <= 1) {
-                    if (!left && c - j >= 0 && !rock[c - j]) {
-                        medusaArea[d][r][c - j] = true;
-                        if (warriorMap[r][c - j] > 0) {
-                            rock[c - j] = true;
-                            kill += warriorMap[r][c - j];
-                            left = true;
-                        }
-                    }
-                    if (!right && c + j < N && !rock[c + j]) {
-                        medusaArea[d][r][c + j] = true;
-                        if (warriorMap[r][c + j] > 0) {
-                            rock[c + j] = true;
-                            kill += warriorMap[r][c + j];
-                            right = true;
-                        }
-                    }
-                } else {
-                    if (!left && r - j >= 0 && !rock[r - j]) {
-                        medusaArea[d][r - j][c] = true;
-                        if (warriorMap[r - j][c] > 0) {
-                            rock[r - j] = true;
-                            kill += warriorMap[r - j][c];
-                            left = true;
-                        }
-                    }
-                    if (!right && r + j < N && !rock[r + j]) {
-                        medusaArea[d][r + j][c] = true;
-                        if (warriorMap[r + j][c] > 0) {
-                            rock[r + j] = true;
-                            kill += warriorMap[r + j][c];
-                            right = true;
-                        }
-                    }
-                }
-            }
-            rr += dr[d];
-            cc += dc[d];
-        }
-
-        if (kill > maxKill || (kill == maxKill && d < dirChosen)) {
-            maxKill = kill;
-            dirChosen = d;
-        }
-    }
-
-    for (int r = 0; r < N; r++)
-        for (int c = 0; c < N; c++)
-            if (medusaArea[dirChosen][r][c] && warriorMap[r][c] > 0) {
-                nextWarriorMap[r][c] = warriorMap[r][c];
-                warriorMap[r][c] = 0;
-            }
-
-    return {maxKill, dirChosen};
+int man_calc_dist(int y1, int x1, int y2, int x2)
+{
+  return abs(y1 - y2) + abs(x2 - x1);
 }
 
-int step3(int turn, int medusaDir) {
-    int sum = 0;
-    pair<int, int> temp[10000];
-    int cnt = 0;
+vector<int> warrior_dir[2] = {
+  {0, 4, 6, 2}, // 상 하 좌 우
+  {6, 2, 0, 4} // 좌 우 상 하 
+};
 
-    for (int i = frontIdx; i < rearIdx; i++) {
-        auto [r, c] = warriorsQueue[i];
-        if (warriorMap[r][c] == 0 || (r == medusaR && c == medusaC)) {
-            temp[cnt++] = {r, c};
-            continue;
-        }
-
-        int originDist = getDist(r, c, medusaR, medusaC);
-        warriorMap[r][c]--;
-
-        int bestDist = 1e9, bestDir = -1, nr = -1, nc = -1;
-        for (int j = (turn == 1 ? 0 : 2); j < (turn == 1 ? 4 : 6); j++) {
-            int d = j % 4;
-            int tr = r + dr[d], tc = c + dc[d];
-            if (!inRange(tr, tc) || medusaArea[medusaDir][tr][tc]) continue;
-            int newDist = getDist(tr, tc, medusaR, medusaC);
-            if (newDist < originDist) {
-                if (newDist < bestDist || (newDist == bestDist && d < bestDir)) {
-                    bestDist = newDist;
-                    bestDir = d;
-                    nr = tr;
-                    nc = tc;
-                }
-            }
-        }
-
-        if (nr == -1) {
-            warriorMap[r][c]++;
-            temp[cnt++] = {r, c};
-        } else {
-            warriorMap[nr][nc]++;
-            temp[cnt++] = {nr, nc};
-            sum++;
-        }
+void warrior_move(int flag)
+{
+  // cout << "이동시작!!!" << endl;
+  // cout << " 메두사는 " << " : " << my << " : " << mx << endl;
+  for (Warrior& w : warrior_vec)
+  {
+    //cout << w.y << " : " << w.x << " : " << w.freeze << " :  " << w.live << endl;
+    if (w.live == 0) {
+      //cout << w.y << " : " << w.x << " 는 죽음" << endl;
+      continue;
     }
-
-    resetWarriorQueue(temp, cnt);
-    return sum;
+    if (w.freeze == 1)
+    {
+      //cout << w.y << " : " << w.x << " 는 얼어있음" << endl;
+      if(flag == 1) w.freeze = 0; 
+      continue;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+      int dir = warrior_dir[flag][i];
+      int ny = w.y + dy[dir];
+      int nx = w.x + dx[dir];
+      if (is_out(ny, nx) || 
+            visited[max_freeze_dir][ny][nx] == 1) continue;
+      if (man_calc_dist(ny, nx, my, mx) + 1 
+            == man_calc_dist(w.y, w.x, my, mx))
+      {
+        //cout << ny << " : " << nx << "로 이동.." << endl;
+        warrior_land[w.y][w.x]--;
+        w.y = ny;
+        w.x = nx;
+        turn_info.warrior_move_sum++;
+        warrior_land[w.y][w.x]++;
+        if (ny == my && nx == mx)
+        {
+          //cout << "장렬히 전사" << endl;
+          w.live = 0;
+          turn_info.warrior_attack_succeed++;
+          warrior_land[w.y][w.x]--;
+        }
+        break;
+      }
+    }
+  }
 }
 
-int step4() {
-    int atk = 0;
-    pair<int, int> temp[10000];
-    int cnt = 0;
-
-    for (int i = frontIdx; i < rearIdx; i++) {
-        auto [r, c] = warriorsQueue[i];
-        if (r == medusaR && c == medusaC) {
-            atk++;
-            warriorMap[r][c]--;
-        } else {
-            temp[cnt++] = {r, c};
-        }
+int main()
+{
+  fastIO();
+  cin >> n >> m;
+  cin >> my >> mx >> py >> px;
+  for (int i = 0; i < m; i++)
+  {
+    int y, x;
+    cin >> y >> x;
+    warrior_vec.push_back({y, x, 0, 1});
+    warrior_land[y][x]++;
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cin >> road[i][j];
     }
+  }
 
-    for (int r = 0; r < N; r++)
-        for (int c = 0; c < N; c++) {
-            while (nextWarriorMap[r][c]--) {
-                warriorMap[r][c]++;
-                temp[cnt++] = {r, c};
-            }
-            nextWarriorMap[r][c] = 0;
-        }
 
-    resetWarriorQueue(temp, cnt);
-    return atk;
-}
+  int a = calc_dist();
+  if (a == -1)
+  {
+    cout << -1 << endl;
+    exit(0);
+  }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+  trace(my, mx);
 
-    cin >> N >> M;
-    cin >> medusaR >> medusaC >> parkR >> parkC;
+  int turn = 0;
 
-    for (int i = 0; i < M; i++) {
-        int r, c;
-        cin >> r >> c;
-        pushWarrior(r, c);
-        warriorMap[r][c]++;
+  while (1)
+  {
+    turn_info = {0, 0, 0};
+    medusa_move(turn); // 전사가 공격못하고 죽음
+    //cout << my << " : " << mx << endl;
+    if (my == py && mx == px)
+    {
+      cout << 0 << endl; return 0;
     }
-
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            cin >> map_[i][j];
-
-    bool visited[50][50] = {};
-
-    while (!(medusaR == parkR && medusaC == parkC)) {
-        if (!step1(visited)) break;
-        auto [killCount, dir] = step2();
-        int moveSum = step3(1, dir) + step3(2, dir);
-        int atk = step4();
-        cout << moveSum << ' ' << killCount << ' ' << atk << '\n';
-    }
-
-    return 0;
+    medusa_look(); // 전사를 얼림
+    warrior_move(0); // 전사가 첫 번째 이동: 공격 후 죽음
+    warrior_move(1); // 전사의 두 번째 이동: 공격 후 죽음
+    cout << turn_info.warrior_move_sum << " " 
+         << turn_info.warrior_freezed << " "
+         << turn_info.warrior_attack_succeed << endl;
+    turn++;
+  }
 }
